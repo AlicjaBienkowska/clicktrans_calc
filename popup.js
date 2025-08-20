b44a987dff8175ddbe7f866baaa9f1ea1cd7b129
AlicjaBienkowska <alicjabienkowskaalicja@gmail.com>
2025-08-20 09:07:50 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,43 +1,40 @@
-
 document.getElementById("buttonCalculate").addEventListener("click", calculate);
 
-function calculate(){
-// Comment : Get page content 
-// const weightText = document.getElementsByClassName("weight")[0].innerText;
-// const amountText = document.getElementsByClassName("amount")[0].innerText;
-// const distanceText = document.getElementsByClassName("distance")[0].children[1].innerText;
+function calculate() {
+  // Comment : Get page content
+  // const weightText = document.getElementsByClassName("weight")[0].innerText;
+  // const amountText = document.getElementsByClassName("amount")[0].innerText;
+  // const distanceText = document.getElementsByClassName("distance")[0].children[1].innerText;
 
-const weightText = "ok. 1030 kg";
-const amountText = "2";
-const distanceText = "635 km";
+  const weightText = "ok. 1030 kg";
+  const amountText = "2";
+  const distanceText = "635 km";
 
-// Comment : Convert weight to value
-// Comment : Remove letters and spaces
-let weightValue = weightText.replace(/\D/g,'');
-weightValue = weightValue.replace(/\s/g, '');
-weightValue = Number(weightValue);
+  // Comment : Convert weight to value
+  // Comment : Remove letters and spaces
+  let weightValue = weightText.replace(/\D/g, "");
+  weightValue = weightValue.replace(/\s/g, "");
+  weightValue = Number(weightValue);
 
-let amountValue = amountText.replace(/\D/g,'');
-amountValue = amountValue.replace(/\s/g, '');
-amountValue = Number(amountValue);
+  let amountValue = amountText.replace(/\D/g, "");
+  amountValue = amountValue.replace(/\s/g, "");
+  amountValue = Number(amountValue);
 
-let distanceValue = distanceText.replace(/\D/g,'');
-distanceValue = distanceValue.replace(/\s/g, '');
-distanceValue = Number(distanceValue);
+  let distanceValue = distanceText.replace(/\D/g, "");
+  distanceValue = distanceValue.replace(/\s/g, "");
+  distanceValue = Number(distanceValue);
 
-// Comment : Calculate number of places on transport truck for calculation
-const singlePlaceLimit = 1400;
-let numberOfPlaces = weightValue / singlePlaceLimit;
-numberOfPlaces = Math.ceil(numberOfPlaces);
+  // Comment : Calculate number of places on transport truck for calculation
+  const singlePlaceLimit = 1400;
+  let numberOfPlaces = weightValue / singlePlaceLimit;
+  numberOfPlaces = Math.ceil(numberOfPlaces);
 
-// Comment : Calculate price
-const pricePerKmEUR = 0.8;
-const shippingPrice = Math.round(numberOfPlaces * pricePerKmEUR * distanceValue);
+  // Comment : Calculate price
+  const pricePerKmEUR = 0.8;
+  const shippingPrice = Math.round(
+    numberOfPlaces * pricePerKmEUR * distanceValue
+  );
 
-console.log("Simulated Price");
-console.log(shippingPrice)
+  console.log("Simulated Price");
+  console.log(shippingPrice);
 }
-
-
-
-
